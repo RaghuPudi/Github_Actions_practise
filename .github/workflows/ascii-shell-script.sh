e5096c44f8b270e8e6af392950328331deddf337
RaghuRajeshPudi <103417332+RaghuPudi@users.noreply.github.com>
2026-07-17 16:22:47 +0530

    Execute the script

--- .github/workflows/ascii-shell-script.sh
+++
@@ -1,6 +0,0 @@
-echo "Installing the ASCII Cowsay script"
-sudo apt-get install cowsay -y
-cowsay -f dog "I am a dog.....BOW! BOW!!!..." >> dog.txt
-cat dog.txt
-ls -lrta
-
---
+++ .github/workflows/shell_script.sh
@@ -0,0 +1,5 @@
+echo "Executing the cowsay script"
+sudo apt-get install cowsay -y
+cowsay -f dragon "I am a dragon....RUN for cover" >> dragon.txt
+cat dragon.txt
+ls -ltra
